8729966ba2dd8d677774142841c76d9065a2ed98
Max Verchynskyi <disablaksteam@gmail.com>
2025-02-13 20:05:15 +0200

    find move methods

--- unity-match-3/Assets/Scripts/Move.cs
+++ unity-match-3/Assets/Scripts/Move.cs
@@ -23,4 +23,69 @@ public struct Move
 			_ => int2(-1, 0)
 		};
 	}
+
+	public static Move FindMove(Match3Game game)
+	{
+		int2 size = game.Size;
+		for (int2 c = 0; c.y < size.y; c.y++)
+		{
+			for (c.x = 0; c.x < size.x; c.x++)
+			{
+				TileState t = game[c];
+
+				if (c.x >= 3 && game[c.x - 2, c.y] == t && game[c.x - 3, c.y] == t)
+					return new Move(c, MoveDirection.Left);
+
+				if (c.x + 3 < size.x && game[c.x + 2, c.y] == t && game[c.x + 3, c.y] == t)
+					return new Move(c, MoveDirection.Right);
+
+				if (c.y >= 3 && game[c.x, c.y - 2] == t && game[c.x, c.y - 3] == t)
+					return new Move(c, MoveDirection.Down);
+
+				if (c.y + 3 < size.y && game[c.x, c.y + 2] == t && game[c.x, c.y + 3] == t)
+					return new Move(c, MoveDirection.Up);
+
+				if (c.y > 1)
+				{
+					if (c.x > 1 && game[c.x - 1, c.y - 1] == t)
+					{
+						if ((c.x >= 2 && game[c.x - 2, c.y - 1] == t) || (c.x + 1 < size.x && game[c.x + 1, c.y - 1] == t))
+							return new Move(c, MoveDirection.Down);
+						if ((c.y >= 2 && game[c.x - 1, c.y - 2] == t) || (c.y + 1 < size.y && game[c.x - 1, c.y + 1] == t))
+							return new Move(c, MoveDirection.Left);
+					}
+
+					if (c.x + 1 < size.x && game[c.x + 1, c.y - 1] == t)
+					{
+						if (c.x + 2 < size.x && game[c.x + 2, c.y - 1] == t)
+							return new Move(c, MoveDirection.Down);
+						if ((c.y >= 2 && game[c.x + 1, c.y - 2] == t) || (c.y + 1 < size.y && game[c.x + 1, c.y + 1] == t))
+							return new Move(c, MoveDirection.Right);
+					}
+				}
+
+
+				if (c.y + 1 < size.y)
+				{
+					if (c.x > 1 && game[c.x - 1, c.y + 1] == t)
+					{
+						if ((c.x >= 2 && game[c.x - 2, c.y + 1] == t) || (c.x + 1 < size.x && game[c.x + 1, c.y + 1] == t))
+							return new Move(c, MoveDirection.Up);
+						if (c.y + 2 < size.y && game[c.x - 1, c.y + 2] == t)
+							return new Move(c, MoveDirection.Left);
+					}
+
+					if (c.x + 1 < size.x && game[c.x + 1, c.y + 1] == t)
+					{
+						if (c.x + 2 < size.x && game[c.x + 2, c.y + 1] == t)
+							return new Move(c, MoveDirection.Up);
+						if (c.y + 2 < size.y && game[c.x + 1, c.y + 2] == t)
+							return new Move(c, MoveDirection.Right);
+					}
+				}
+			}
+		}
+
+		return default;
+	}
 }
